385d006229c4d856295788d7c5c12352a0acf4e6
ashishb1224 <ashishbhardwaj14022002@gmail.com>
2025-11-15 11:42:27 +0530

    add new images and update image references; enhance CSS styles and implement Swiper functionality

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ menuOpenButton.addEventListener('click', () => {
 });
 menuCloseButton.addEventListener('click', () => menuOpenButton.click()
 );
+ //initialize swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    DynamicBullets: true,
+},
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
